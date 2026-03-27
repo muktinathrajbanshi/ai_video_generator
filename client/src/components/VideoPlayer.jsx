4a664ab5216726cc359cae1d43c551b4960e2ce8
unknown <muktinathrajbanshi786@gmail.com>
2026-03-27 10:58:05 +0545

    similarly modified on video player file

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,7 +1,9 @@
 function VideoPlayer({ video }) {
+  if (!video) return null;
+
   return (
-    <div className="card">
-      <video src={video} controls style={{ width: '100%', borderRadius: '15px' }} />
+    <div className="card" style={{ maxWidth: "500px", margin: "20px auto" }}>
+      <video src={video} controls style={{ width: "100%", borderRadius: "15px" }} />
     </div>
   );
 }
