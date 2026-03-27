818e5a46969b00bc6254e35d76a6d9fc11a9e169
unknown <muktinathrajbanshi786@gmail.com>
2026-03-27 14:16:45 +0545

    wow now it's time for security

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const { generateVideo } = require("../controllers/videoController");
+const { generateVideo } = require("../controllers/videoControllers");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
